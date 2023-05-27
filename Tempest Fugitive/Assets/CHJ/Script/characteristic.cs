cc57fa4577d07966585e4912b034446f3307ec98
CHJ000110 <chj000110@kakao.com>
2023-05-27 22:25:19 +0900

    삭제

--- Tempest Fugitive/Assets/CHJ/Script/characteristic.cs
+++ Tempest Fugitive/Assets/CHJ/Script/characteristic.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class characteristic : MonoBehaviour
+public class characteristic: MonoBehaviour
 {
     // Start is called before the first frame update
     public string player;
